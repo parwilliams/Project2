40e9f865016889963b8203824e6fa4e5968d071b
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-09 13:37:05 -0500

    Wrote indexOfStation()

--- PosAvg.java
+++ PosAvg.java
@@ -1,3 +1,6 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class PosAvg {
 	
@@ -6,11 +9,30 @@ public class PosAvg {
 	
 	public PosAvg(String id){
 		stationID = id;
+		
+		
 	}
 	
-	public String indexOfStation() {
+	public String indexOfStation() throws IOException{
 		//TODO: Implement
+		FileInputStream fileByteStream = new FileInputStream("Mesonet.txt");
+		Scanner scnr = new Scanner(fileByteStream);
+		boolean exitWhile = false;
+		for (int i = 0; i < 3; i++) {
+			scnr.nextLine();
+		}
 		
+		do {
+			String lineOfData = scnr.nextLine();
+			String indexCheck = lineOfData.substring(1, 4);
+			
+			if(indexCheck.contentEquals(stationID)) {
+				index = stationID;
+				exitWhile = true;
+			}
+		}
+		while(exitWhile == false);
+		fileByteStream.close();
 		return index;
 	}
 	
