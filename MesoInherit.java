7a8c852ac0b21d8969c87424c2fd4b325a2bb458
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-09 17:46:05 -0500

    Defined calAverage()

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,14 +1,47 @@
 
-public class MesoInherit extends MesoAbstract{
+public class MesoInherit extends MesoAbstract {
 
-	
+	String stationID = null;
+
 	public MesoInherit(MesoStation station) {
 		// TODO Auto-generated constructor stub
+		stationID = station.getStID();
 	}
+
 	@Override
 	int[] calAverage() {
 		// TODO Auto-generated method stub
-		return null;
+		int[] asciiCharacters = new int[4];
+		int[] asciiAverage = new int[3];
+		int ceiling = -1;
+		int floor = 1000;
+		int average = -1;
+		double sumOfCharacters = 0;
+		double trueAverage;
+
+		for (int i = 0; i < 4; i++) { // Loop to put character values in array
+			asciiCharacters[i] = stationID.charAt(i);
+		}
+
+		for (int i = 0; i < 4; i++) { // Add All characters
+			sumOfCharacters += asciiCharacters[i];
+		}
+
+		trueAverage = sumOfCharacters / 4;
+
+		ceiling = (int) Math.ceil(trueAverage);
+		floor = (int) Math.floor(trueAverage);
+
+		if ((sumOfCharacters % 4) >= 2) {
+			average = ceiling;
+		} else
+			average = floor;
+
+		asciiAverage[0] = ceiling;
+		asciiAverage[1] = floor;
+		asciiAverage[2] = average;
+
+		return asciiAverage;
 	}
 
 	@Override
@@ -16,5 +49,5 @@ public class MesoInherit extends MesoAbstract{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 }
