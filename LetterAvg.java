52c367d139356bce03b7bd1d45ee13ef2fbc5e7a
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-09 18:55:27 -0500

    Defined toString()

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,31 +1,45 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LetterAvg {
 
 	private char avgLetter = ',';
-
+	private ArrayList<String> matchingStations = new ArrayList<String>();
+	
 	public LetterAvg(char letter) {
 		avgLetter = letter;
 	}
-
+	
+	
 	public int numberOfStationWithLetterAvg() throws FileNotFoundException {
+		String station;
 		char fileChar;
-		int matchingStations = 0;// Number of Stations starting with the average letter
+		int numOfMatchingStations = 0;// Number of Stations starting with the average letter
 		FileInputStream fileByteStream = new FileInputStream("Mesonet.txt");
 		Scanner scnr = new Scanner(fileByteStream);
 		for (int i = 0; i < 3; i++) { // Dismisses first 3 lines
 			scnr.nextLine();
 		}
 		while (scnr.hasNextLine()) {
-			fileChar = scnr.nextLine().charAt(1);
+			station = scnr.nextLine().substring(1,5);
+			fileChar = station.charAt(0);
 			if (fileChar == avgLetter) {
-				++matchingStations;
+				++numOfMatchingStations;
+				matchingStations.add(station);
 			}
 		}
 		scnr.close();
-		return matchingStations;
+		return numOfMatchingStations;
 	}
-
+	
+	@Override
+	public String toString() {
+		String str = "They are:\n";
+		for(int i = 0; i < matchingStations.size(); i++) {
+			str = str.concat(matchingStations.get(i) + "\n");
+		}
+		return str;
+	}
 }
