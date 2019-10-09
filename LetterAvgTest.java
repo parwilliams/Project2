d6a1e29bf75bbc0fcd0c801c197d293fd820f34c
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-09 08:53:35 -0500

    Test Templates

---
+++ LetterAvgTest.java
@@ -0,0 +1,66 @@
+
+/**
+ * 
+ * @author Parker Williams
+ * @version 2019-10-09
+ * Letter Average Tests
+ */
+public class LetterAvgTest {
+	/*public void emptyConstructorTest() throws AssertException {
+	
+	//Use the default constructor
+	From Previous projects
+	Animal ani = new Animal();
+	 
+	
+	//The name should be "noname", the height and weight 0, and the color "unknown"
+	Assert.assertEquals(0.0, ani.getWeight(),0.0);
+	Assert.assertEquals("noname", ani.getName());
+	Assert.assertEquals("unknown", ani.getColor());
+	Assert.assertEquals(0, ani.getHeight(),0.0);
+	
+	}
+
+	public void AnimalConstructorTest() throws AssertException
+	{
+		//Use the multi-parameter constructor
+		//Animal ani = new Animal("Brown","Billy",132.0,12.0);
+		Assert.assertEquals("Brown", ani.getColor());
+		Assert.assertEquals("Billy",ani.getName());
+		Assert.assertEquals(132.0,ani.getWeight(),0.0);
+		Assert.assertEquals(12.0, ani.getHeight(),0.0);
+	
+	}
+
+	public void getWeight() throws AssertException
+	{
+		Animal ani = new Animal("Brown","Billy",132.0,12.0);
+		double expected= ani.getWeight();
+		double actual = 132.0;
+		Assert.assertEquals(expected, actual,0.0);		
+	}
+
+	public void getHeight() throws AssertException
+	{
+		Animal ani = new Animal("Brown","Billy",132.0,12.0);
+		double expected= ani.getHeight();
+		double actual = 12.0;
+		Assert.assertEquals(expected, actual,0.0);		
+	}
+
+	public void getColor() throws AssertException
+	{
+		Animal ani = new Animal("Brown","Billy",132.0,12.0);
+		String expected = ani.getColor();
+		String actual = "Brown";
+		Assert.assertEquals(expected, actual);		
+	}
+
+	public void testToString() throws AssertException
+	{
+		Animal ani = new Animal("Brown","Billy",132.0,12.0);
+		String expected = ani.toString();
+		String actual = "Billy: a Brown-colored animal - 132.0 pounds and 12.0 inches.\n";
+		Assert.assertEquals(expected, actual);
+	}*/
+}
