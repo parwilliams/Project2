d2ddfbf72a8f3f6cc7f300140f00b2931f59ba55
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-09 16:39:47 -0500

    Finished toString()

--- PosAvg.java
+++ PosAvg.java
@@ -37,10 +37,31 @@ public class PosAvg {
 	}
 	
 	@Override
-	public String toString() {
-		String str = null;
-		
-		
+	public String toString(){
+		String indexPlusOne;
+		String indexPlusTwo;
+		String indexMinusOne;
+		String indexMinusTwo;
+		String str = "";
+		try {  //Java does not allow throwing exceptions on toString() method
+			FileInputStream fileByteStream = new FileInputStream("Mesonet.txt");
+			Scanner scnr = new Scanner(fileByteStream);
+			boolean exitWhile = false;
+			for (int i = 0; i < index; i++) {
+				scnr.nextLine();
+			}
+			
+			indexMinusTwo = scnr.nextLine().substring(1,5);
+			indexMinusOne = scnr.nextLine().substring(1,5);
+			scnr.nextLine();//Line of index
+			indexPlusOne = scnr.nextLine().substring(1,5);
+			indexPlusTwo = scnr.nextLine().substring(1,5);
+			
+			str = String.format("This index is the average of %s and %s, %s and %s, and so on.", indexMinusOne, indexPlusOne, indexMinusTwo, indexPlusTwo);
+		}
+		catch(Exception e) {
+			e.toString();
+		}
 		
 		return str;
 	}
