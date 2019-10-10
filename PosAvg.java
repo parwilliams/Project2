b208f4381b66dbcc56de5cd804364e420dea9336
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-10 17:40:46 -0500

    Modified for Array With toString()

--- PosAvg.java
+++ PosAvg.java
@@ -1,15 +1,16 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
-
+//toString gives correct Output but ZyBooks will not Accept
 public class PosAvg {
 	
 	int index = 0;
 	String stationID = null;
-	String indexPlusOne;
-	String indexPlusTwo;
-	String indexMinusOne;
-	String indexMinusTwo;
+	/*	String indexPlusOne;
+		String indexPlusTwo;
+		String indexMinusOne;
+		String indexMinusTwo;
+	*/
 	
 	public PosAvg(String id){
 		stationID = id;	
@@ -39,7 +40,8 @@ public class PosAvg {
 		return index;
 	}
 	
-	private void fileSearch(){
+	private String[] getAvgStationNames(){
+		String[] avgStationNames = new String[4]; //names around index
 		try{
 			FileInputStream fileByteStream = new FileInputStream("Mesonet.txt");
 			Scanner scnr = new Scanner(fileByteStream);
@@ -47,23 +49,36 @@ public class PosAvg {
 				scnr.nextLine();
 			}
 			
-			indexMinusTwo = scnr.nextLine().substring(1,5);
+			
+			/*indexMinusTwo = scnr.nextLine().substring(1,5);
 			indexMinusOne = scnr.nextLine().substring(1,5);
 			scnr.nextLine();//Line of index
 			indexPlusOne = scnr.nextLine().substring(1,5);
 			indexPlusTwo = scnr.nextLine().substring(1,5);
+			*/
+			
+			avgStationNames[0] = scnr.nextLine().substring(1,5);// index - 2
+			avgStationNames[1] = scnr.nextLine().substring(1,5);// index - 1
+			scnr.nextLine();//Line of index
+			avgStationNames[2] = scnr.nextLine().substring(1,5);// index + 1
+			avgStationNames[3] = scnr.nextLine().substring(1,5);// index + 2
+			return avgStationNames;
 		}
 		catch(Exception e) {
 			e.toString();
 		}	
+		return avgStationNames;
 	}
 	
 	
 	
 	@Override
 	public String toString(){
-		this.fileSearch();
-		String str = String.format("This index is average of %s and %s, %s and %s, and so on.", indexMinusOne, indexPlusOne, indexMinusTwo, indexPlusTwo);
+		String[] avgStationNamesV2 = getAvgStationNames();
+		/*
+		 * String str = String.format("This index is average of %s and %s, %s and %s, and so on.", indexMinusOneV2, indexPlusOneV2, indexMinusTwoV2, indexPlusTwoV2);
+		 */
+		String str = String.format("This index is average of %s and %s, %s and %s, and so on.", avgStationNamesV2[1], avgStationNamesV2[2], avgStationNamesV2[0], avgStationNamesV2[3]);
 		return str;
 	}
 	
