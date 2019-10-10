aad48b16b098396b9d6cb3bec0b88f459ced0c3d
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-10 14:51:08 -0500

    Basic Test

--- LetterAvgTest.java
+++ LetterAvgTest.java
@@ -6,61 +6,22 @@
  * Letter Average Tests
  */
 public class LetterAvgTest {
-	/*public void emptyConstructorTest() throws AssertException {
+	String stID = "OKCE";
+	MesoInherit mesoInherit = new MesoInherit(new MesoStation(stID));
+	LetterAvg letterAvg = new LetterAvg(mesoInherit.letterAverage()); 
 	
-	//Use the default constructor
-	From Previous projects
-	Animal ani = new Animal();
-	 
-	
-	//The name should be "noname", the height and weight 0, and the color "unknown"
-	Assert.assertEquals(0.0, ani.getWeight(),0.0);
-	Assert.assertEquals("noname", ani.getName());
-	Assert.assertEquals("unknown", ani.getColor());
-	Assert.assertEquals(0, ani.getHeight(),0.0);
-	
+	public void numberOfStationWithLetterAvgTest() throws AssertException {
+		try{
+			Assert.assertEquals(2, letterAvg.numberOfStationWithLetterAvg());	
+		}
+		catch(Exception e) {
+			e.toString();
+		}
 	}
 
-	public void AnimalConstructorTest() throws AssertException
+	public void toStringTest() throws AssertException
 	{
-		//Use the multi-parameter constructor
-		//Animal ani = new Animal("Brown","Billy",132.0,12.0);
-		Assert.assertEquals("Brown", ani.getColor());
-		Assert.assertEquals("Billy",ani.getName());
-		Assert.assertEquals(132.0,ani.getWeight(),0.0);
-		Assert.assertEquals(12.0, ani.getHeight(),0.0);
-	
+		Assert.assertEquals("\nThey are:\n" + "IDAB\n" + "INOL", letterAvg.toString());
 	}
 
-	public void getWeight() throws AssertException
-	{
-		Animal ani = new Animal("Brown","Billy",132.0,12.0);
-		double expected= ani.getWeight();
-		double actual = 132.0;
-		Assert.assertEquals(expected, actual,0.0);		
-	}
-
-	public void getHeight() throws AssertException
-	{
-		Animal ani = new Animal("Brown","Billy",132.0,12.0);
-		double expected= ani.getHeight();
-		double actual = 12.0;
-		Assert.assertEquals(expected, actual,0.0);		
-	}
-
-	public void getColor() throws AssertException
-	{
-		Animal ani = new Animal("Brown","Billy",132.0,12.0);
-		String expected = ani.getColor();
-		String actual = "Brown";
-		Assert.assertEquals(expected, actual);		
-	}
-
-	public void testToString() throws AssertException
-	{
-		Animal ani = new Animal("Brown","Billy",132.0,12.0);
-		String expected = ani.toString();
-		String actual = "Billy: a Brown-colored animal - 132.0 pounds and 12.0 inches.\n";
-		Assert.assertEquals(expected, actual);
-	}*/
 }
