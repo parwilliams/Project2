e3c28fb201bcbeead75283209fb022c4689d96f1
Parker Williams <williparl@Parkers-MacBook-Pro.local>
2019-10-09 21:05:55 -0500

    Added fileSearch()

--- PosAvg.java
+++ PosAvg.java
@@ -6,11 +6,13 @@ public class PosAvg {
 	
 	int index = 0;
 	String stationID = null;
+	String indexPlusOne;
+	String indexPlusTwo;
+	String indexMinusOne;
+	String indexMinusTwo;
 	
 	public PosAvg(String id){
-		stationID = id;
-		
-		
+		stationID = id;	
 	}
 	
 	public int indexOfStation() throws IOException{
@@ -32,21 +34,15 @@ public class PosAvg {
 			}
 		}
 		while(exitWhile == false);
+		
 		fileByteStream.close();
 		return index;
 	}
 	
-	@Override
-	public String toString(){
-		String indexPlusOne;
-		String indexPlusTwo;
-		String indexMinusOne;
-		String indexMinusTwo;
-		String str = "";
-		try {  //Java does not allow throwing exceptions on toString() method
+	private void fileSearch(){
+		try{
 			FileInputStream fileByteStream = new FileInputStream("Mesonet.txt");
 			Scanner scnr = new Scanner(fileByteStream);
-			boolean exitWhile = false;
 			for (int i = 0; i < index; i++) {
 				scnr.nextLine();
 			}
@@ -56,13 +52,18 @@ public class PosAvg {
 			scnr.nextLine();//Line of index
 			indexPlusOne = scnr.nextLine().substring(1,5);
 			indexPlusTwo = scnr.nextLine().substring(1,5);
-			
-			str = String.format("This index is the average of %s and %s, %s and %s, and so on.", indexMinusOne, indexPlusOne, indexMinusTwo, indexPlusTwo);
 		}
 		catch(Exception e) {
 			e.toString();
-		}
-		
+		}	
+	}
+	
+	
+	
+	@Override
+	public String toString(){
+		this.fileSearch();
+		String str = String.format("This index is average of %s and %s, %s and %s, and so on.", indexMinusOne, indexPlusOne, indexMinusTwo, indexPlusTwo);
 		return str;
 	}
 	
